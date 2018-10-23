764209dd11c04614cd292be8d753d0c600223aaf
Philipp Klaus <philipp.l.klaus@web.de>
2018-10-23 14:26:44 +0200

    removed TrbNet._get_scalar_endpoint_dict()
    It's redundant as it's basically a border case of the more general
    TrbNet._get_dynamic_endpoint_dict() where the data length is fixed to 1.

--- trbnet/highlevel.py
+++ trbnet/highlevel.py
@@ -9,13 +9,16 @@ class TrbNet(_TrbNet):
 
     def trb_register_read(self, trb_address, reg_address):
         lin_data = super().trb_register_read(trb_address, reg_address)
-        return self._get_scalar_endpoint_dict(lin_data)
+        if (len(lin_data) % 2) != 0:
+            raise ValueError("len(lin_data) == %d -  expected a multiple of %d" % (len(lin_data), 2))
+        result = self._get_dynamic_endpoint_dict(lin_data, force_length=1)
+        return {key: value[0] for key, value in result.items()}
 
     def trb_register_read_mem(self, trb_address, reg_address, option, size):
         lin_data = super().trb_register_read_mem(trb_address, reg_address, option, size)
         return self._get_dynamic_endpoint_dict(lin_data)
 
-    def _get_dynamic_endpoint_dict(self, lin_data):
+    def _get_dynamic_endpoint_dict(self, lin_data, force_length=0):
         """
         A utility function to structure response data from the
         trb_register_read() and trb_register_read_mem() functions.
@@ -33,28 +36,7 @@ class TrbNet(_TrbNet):
             header = lin_data[offset]
             offset += 1
             length, endpoint = (header >> 16), (header & 0xffff)
+            if force_length: length = force_length
             endpoint_dict[endpoint] = lin_data[offset:offset+length]
             offset += length
-        return endpoint_dict
-
-    def _get_scalar_endpoint_dict(self, lin_data):
-        """
-        A utility function to structure response data from the
-        trb_register_read() and trb_register_read_mem() functions.
-        As multiple endpoints can reply to a single request, it
-        splits the linar response into the data from each eandpoint.
-        Returns a dictionary with the endpoints as keys and the
-        respective scalar data as value.
-
-        Returns:
-        dict -- key: endpoint trb_address, value: int (32-bit word)
-        """
-        endpoint_dict = {}
-        if (len(lin_data) % 2) != 0:
-            raise ValueError("len(lin_data) =", len(lin_data), " -  expected a multiple of", 2)
-        endpoints = lin_data[::2]
-        offset = 0
-        for endpoint in endpoints:
-            endpoint_dict[endpoint] = lin_data[offset + 1]
-            offset += 2
         return endpoint_dict
