4e13549a1572a0ab9368a78d4227818c472c8a0e
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2021-05-18 15:41:03 +0200

    setuptools-scm not strictly necessary

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 from setuptools import setup
 
 setup(
-    use_scm_version={
-        'version_scheme': 'post-release',
-        'local_scheme': 'dirty-tag'
-    },
     setup_requires=[
-        'setuptools_scm >= 1.7.0',
         'setuptools >= 38.6.0'
     ]
 )
